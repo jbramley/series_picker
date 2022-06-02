7e4db23430aa98fec671f21cfc92e74ba63fbff7
Justin Bramley <jbramley@gmail.com>
2022-06-02 16:31:27 -0400

    Minor reformatting

--- src/fetchers/__init__.py
+++ src/fetchers/__init__.py
@@ -1,5 +1,4 @@
 from fetchers._goodreads import goodreads_fetcher
+from fetchers._yaml import yaml_fetcher
 
 __all__ = ["goodreads_fetcher", "yaml_fetcher"]
-
-from fetchers._yaml import yaml_fetcher
